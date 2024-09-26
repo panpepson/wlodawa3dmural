d6d6241d0a43082ab99b5fd0e65924d090e272f3
p.trochymiak <piotr@trochymiak.net>
2024-09-26 13:16:11 +0200

    koloryzacja max

--- app.js
+++ app.js
@@ -24,9 +24,14 @@ function drawVideoToCanvas() {
     const data = imageData.data;
     for (let i = 0; i < data.length; i += 4) {
         if (kolor === 'red') {
- data[i] = Math.min(data[i] + 200, 255);   // Czerwony nie przekracza 255
-    data[i + 1] = Math.max(data[i + 1] - 5, 0);   // Zielony nie spada poniżej 0
-    data[i + 2] = Math.max(data[i + 2] - 1, 0); 
+//  data[i] = Math.min(data[i] + 200, 255);   // Czerwony nie przekracza 255
+//     data[i + 1] = Math.max(data[i + 1] - 5, 0);   // Zielony nie spada poniżej 0
+//     data[i + 2] = Math.max(data[i + 2] - 1, 0); 
+
+    data[i] = 255;
+    data[i + 1] = 0;
+    data[i + 2] = 0;
+
         } else if (kolor === 'blue') {
            data[i] = Math.max(data[i] - 5, 0);         // Czerwony kanał (R), nie spada poniżej 0
     data[i + 1] = Math.max(data[i + 1] - 5, 0); // Zielony kanał (G), nie spada poniżej 0
