60d58133282db12b26283bc5f977a19ff3712917
p.trochymiak <piotr@trochymiak.net>
2024-09-26 13:11:00 +0200

    koloryzacjaograniczenia

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ function drawVideoToCanvas() {
     for (let i = 0; i < data.length; i += 4) {
         if (kolor === 'red') {
  data[i] = Math.min(data[i] + 200, 255);   // Czerwony nie przekracza 255
-    data[i + 1] = Math.max(data[i + 1] - 50, 0);   // Zielony nie spada poniżej 0
+    data[i + 1] = Math.max(data[i + 1] - 5, 0);   // Zielony nie spada poniżej 0
     data[i + 2] = Math.max(data[i + 2] - 1, 0); 
         } else if (kolor === 'blue') {
-           data[i] = Math.max(data[i] - 50, 0);         // Czerwony kanał (R), nie spada poniżej 0
-    data[i + 1] = Math.max(data[i + 1] - 50, 0); // Zielony kanał (G), nie spada poniżej 0
+           data[i] = Math.max(data[i] - 5, 0);         // Czerwony kanał (R), nie spada poniżej 0
+    data[i + 1] = Math.max(data[i + 1] - 5, 0); // Zielony kanał (G), nie spada poniżej 0
     data[i + 2] = Math.min(data[i + 2] + 100, 255); // Niebieski kanał (B), nie przekracza 255
 
         } else if (kolor === 'normal') {
